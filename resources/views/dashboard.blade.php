fb1d52f3716870ed99294096a01c160f5eac9a2e
mazn91 <mazn.adnan.ma@gmail.com>
2018-03-22 16:49:34 +0300

    bootstrap template commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,247 @@
+
+
+@extends('layouts.master')
+
+
+@section('content')
+
+        <div class="breadcrumbs">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    <div class="page-title">
+                        <h1>Dashboard</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="page-header float-right">
+                    <div class="page-title">
+                        <ol class="breadcrumb text-right">
+                            <li class="active">Dashboard</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="col-sm-12 mb-4">
+            <div class="card-group">
+                <div class="card col-md-6 no-padding ">
+                    <div class="card-body">
+                        <div class="h1 text-muted text-right mb-4">
+                            <i class="fa fa-users"></i>
+                        </div>
+
+                        <div class="h4 mb-0">
+                            <span class="count">87500</span>
+                        </div>
+
+                        <small class="text-muted text-uppercase font-weight-bold">Visitors</small>
+                        <div class="progress progress-xs mt-3 mb-0 bg-flat-color-1" style="width: 40%; height: 5px;"></div>
+                    </div>
+                </div>
+                <div class="card col-md-6 no-padding ">
+                    <div class="card-body">
+                        <div class="h1 text-muted text-right mb-4">
+                            <i class="fa fa-user-plus"></i>
+                        </div>
+                        <div class="h4 mb-0">
+                            <span class="count">385</span>
+                        </div>
+                        <small class="text-muted text-uppercase font-weight-bold">New Clients</small>
+                        <div class="progress progress-xs mt-3 mb-0 bg-flat-color-2" style="width: 40%; height: 5px;"></div>
+                    </div>
+                </div>
+                <div class="card col-md-6 no-padding ">
+                    <div class="card-body">
+                        <div class="h1 text-muted text-right mb-4">
+                            <i class="fa fa-cart-plus"></i>
+                        </div>
+                        <div class="h4 mb-0">
+                            <span class="count">1238</span>
+                        </div>
+                        <small class="text-muted text-uppercase font-weight-bold">Products sold</small>
+                        <div class="progress progress-xs mt-3 mb-0 bg-flat-color-3" style="width: 40%; height: 5px;"></div>
+                    </div>
+                </div>
+                <div class="card col-md-6 no-padding ">
+                    <div class="card-body">
+                        <div class="h1 text-muted text-right mb-4">
+                            <i class="fa fa-pie-chart"></i>
+                        </div>
+                        <div class="h4 mb-0">
+                            <span class="count">28</span>%
+                        </div>
+                        <small class="text-muted text-uppercase font-weight-bold">Returning Visitors</small>
+                        <div class="progress progress-xs mt-3 mb-0 bg-flat-color-4" style="width: 40%; height: 5px;"></div>
+                    </div>
+                </div>
+                <div class="card col-md-6 no-padding ">
+                    <div class="card-body">
+                        <div class="h1 text-muted text-right mb-4">
+                            <i class="fa fa-clock-o"></i>
+                        </div>
+                        <div class="h4 mb-0">5:34:11</div>
+                        <small class="text-muted text-uppercase font-weight-bold">Avg. Time</small>
+                        <div class="progress progress-xs mt-3 mb-0 bg-flat-color-5" style="width: 40%; height: 5px;"></div>
+                    </div>
+                </div>
+                <div class="card col-md-6 no-padding ">
+                    <div class="card-body">
+                        <div class="h1 text-muted text-right mb-4">
+                            <i class="fa fa-comments-o"></i>
+                        </div>
+                        <div class="h4 mb-0">
+                            <span class="count">972</span>
+                        </div>
+                        <small class="text-muted text-uppercase font-weight-bold">COMMENTS</small>
+                        <div class="progress progress-xs mt-3 mb-0 bg-flat-color-1" style="width: 40%; height: 5px;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="stat-widget-one">
+                                    <div class="stat-icon dib"><i class="ti-money text-success border-success"></i></div>
+                                    <div class="stat-content dib">
+                                        <div class="stat-text">Total Profit</div>
+                                        <div class="stat-digit">1,012</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="stat-widget-one">
+                                    <div class="stat-icon dib"><i class="ti-user text-primary border-primary"></i></div>
+                                    <div class="stat-content dib">
+                                        <div class="stat-text">New Customer</div>
+                                        <div class="stat-digit">961</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="stat-widget-one">
+                                    <div class="stat-icon dib"><i class="ti-layout-grid2 text-warning border-warning"></i></div>
+                                    <div class="stat-content dib">
+                                        <div class="stat-text">Active Projects</div>
+                                        <div class="stat-digit">770</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="stat-widget-one">
+                                    <div class="stat-icon dib"><i class="ti-link text-danger border-danger"></i></div>
+                                    <div class="stat-content dib">
+                                        <div class="stat-text">Referrals</div>
+                                        <div class="stat-digit">2,781</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="stat-widget-four">
+                                    <div class="stat-icon dib">
+                                        <i class="ti-server text-muted"></i>
+                                    </div>
+                                    <div class="stat-content">
+                                        <div class="text-left dib">
+                                            <div class="stat-heading">Database</div>
+                                            <div class="stat-text">Total: 765</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="stat-widget-four">
+                                    <div class="stat-icon dib">
+                                        <i class="ti-user text-muted"></i>
+                                    </div>
+                                    <div class="stat-content">
+                                        <div class="text-left dib">
+                                            <div class="stat-heading">Users</div>
+                                            <div class="stat-text">Total: 24720</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="stat-widget-four">
+                                    <div class="stat-icon dib">
+                                        <i class="ti-stats-up text-muted"></i>
+                                    </div>
+                                    <div class="stat-content">
+                                        <div class="text-left dib">
+                                            <div class="stat-heading">Daily Sales</div>
+                                            <div class="stat-text">Total: $76,58,714</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="stat-widget-four">
+                                    <div class="stat-icon dib">
+                                        <i class="ti-pulse text-muted"></i>
+                                    </div>
+                                    <div class="stat-content">
+                                        <div class="text-left dib">
+                                            <div class="stat-heading">Bandwidth</div>
+                                            <div class="stat-text">Total: 4TB</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+ 
+
+
+
+
+
+@endsection
+
+
+
+
+
+                    
---
+++ storage/framework/views/266ad43f9b1db66431609acafeeea842e2d42de8.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sufee Admin - HTML5 Admin Template</title>
+    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-icon.png">
+    <link rel="shortcut icon" href="favicon.ico">
+
+
+
+
+
+    <link href="<?php echo e(asset('css/assets/css/normalize.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('css/assets/css/bootstrap.min.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('css/assets/css/font-awesome.min.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('css/assets/css/themify-icons.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('css/assets/css/flag-icon.min.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('css/assets/css/cs-skin-elastic.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('css/assets/scss/style.css')); ?>" rel="stylesheet">
+
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+
+</head>
+<body class="bg-dark">
+
+
+    <div class="sufee-login d-flex align-content-center flex-wrap">
+        <div class="container">
+            <div class="login-content">
+                <div class="login-logo">
+                    <a href="index.html">
+                        <img class="align-content" src="images/logo.png" alt="">
+                    </a>
+                </div>
+                <div class="login-form">
+                    <form>
+                        <div class="form-group">
+                            <label>Email address</label>
+                            <input type="email" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control" placeholder="Password">
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox"> Remember Me
+                            </label>
+                            <label class="pull-right">
+                                <a href="#">Forgotten Password?</a>
+                            </label>
+
+                        </div>
+                        <button type="submit" class="btn btn-success btn-flat m-b-30 m-t-30">Sign in</button>
+                       
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+        <script type="text/javascript" src="<?php echo asset('js/assets/js/vendor/jquery-2.1.4.min.j'); ?>"></script>
+        <script type="text/javascript" src="<?php echo asset('js/assets/js/popper.min.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo asset('js/assets/js/plugins.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo asset('js/assets/js/main.js'); ?>"></script>
+
+
+</body>
+</html>
